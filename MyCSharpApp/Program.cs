f0d02cb98fb1569c74740cdb111120ed37a1dddd
Elizabeth <lizbeepmc@gmail.com>
2024-05-01 15:39:23 +0300

    ıntro to csharp

---
+++ MyCSharpApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("this is my world");
+Console.WriteLine("this is it");
+Console.Write("hello hello");
+//Data types
+Console.Write("hello"); //string literal more than one character with double quotes
+Console.WriteLine('b'); //char literal just one character with single Quotes
+Console.WriteLine(123); //int literal also ınteger
+//float laterals
+Console.WriteLine(0.25F); //you can make the f small or bıg thıs ıs one of the float types
+Console.WriteLine(2.625); //thıs ıs a double float type. If you do not attach F to the end the compiler assumes ıts a double float type
+Console.WriteLine(12.39816m); //this ia a decimal float type. you can use capıtal m or small m . thıs has the best precısıon.Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine(true); //If you wanted to print a value representing either true or false, you could use a bool literal.The term bool is short for Boolean. In C#, they're officially referred to as "bool", but often developers use the term "Boolean".
+//Declaring variable
+string firstName; //to declare a varıable you declare the datatype and the varıable name In this case, you're creating a new variable of type string called firstName. From now on, this variable can only hold string values.You can choose any name as long as it adheres to a few C# syntax rules for naming variables.
